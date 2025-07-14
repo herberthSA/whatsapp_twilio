93ba8cca274059a52b8cacf54d917ff7fc8c4a8d
Herberth Orlando Mancía Cisneros <herberthmeca@gmail.com>
2025-07-14 16:35:06 -0600

    fix - formato de console log

--- server.js
+++ server.js
@@ -1,27 +1,24 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import axios from 'axios';
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post('/webhook', async (req, res) => {
- console.log(req.body)
-  
+  console.log(req.body)
+  try {
+    if (req.body.Latitude && req.body.Longitude) {
+      console.log('Latitud:', req.body.Latitude);
+      console.log('Longitud:', req.body.Longitude);
+      const googleMapsUrl = `https://www.google.com/maps?q=${req.body.Latitude},${req.body.Longitude}`;
+      console.log('Abrir mapa en:', googleMapsUrl);
+    }
+    console.log(req.body.Body)
 
-   try {
-      if(req.body.Latitude && req.body.Longitude) {
-        console.log('Latitud:', req.body.Latitude);
-        console.log('Longitud:', req.body.Longitude);
-        const googleMapsUrl = `https://www.google.com/maps?q=${req.body.Latitude},${req.body.Longitude}`;
-        console.log('Abrir mapa en:', googleMapsUrl);
-      }
-      console.log(req.body.Body)
-      
 
-    } catch (error) {
-      console.error('Error al obtener ubicación:', error);
-    }  
+  } catch (error) {
+    console.error('Error al obtener ubicación:', error);
+  }
 });
 
 app.listen(3000, () => console.log('Servidor escuchando en http://localhost:3000'));
